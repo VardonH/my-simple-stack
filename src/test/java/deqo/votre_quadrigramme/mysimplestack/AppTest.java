a4c2f945c90680087ca6d43544f036bc211f8715
VardonH <hugo.vardon@etud.univ-jfc.fr>
2020-10-22 14:38:05 +0200

    first commit[

---
+++ src/test/java/deqo/votre_quadrigramme/mysimplestack/AppTest.java
@@ -0,0 +1,20 @@
+package deqo.votre_quadrigramme.mysimplestack;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
